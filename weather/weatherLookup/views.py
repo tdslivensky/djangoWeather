65c522c3affdb82dc757582de8d6ea59b5d383b6
Timothy Slivensky <tdslivensky@gmail.com>
2020-03-01 22:56:57 -0600

    got a js object to screen

--- weather/weatherLookup/views.py
+++ weather/weatherLookup/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
-	return render(request, 'Home.html', {})
+	import json 
+	import requests
+	#https://stackoverflow.com/questions/42658285/convert-bool-values-to-string-in-json-dumps
+	def convert(obj):
+	    if isinstance(obj, bool) or obj == None:
+	    	if obj == None:
+	    		obj = 'null'
+	    	return str(obj).lower()
+	    if isinstance(obj, (list, tuple)):
+	        return [convert(item) for item in obj]
+	    if isinstance(obj, dict):
+	        return {convert(key):convert(value) for key, value in obj.items()}
+	    return obj
+
+	apiTemperature = requests.get("https://api.weather.gov/gridpoints/EWX/157,96/forecast")
+	try:
+		api = json.loads(apiTemperature.content)
+		api = convert(api)
+	except Exception as e:
+		api = 'No information returned by api'
+	return render(request, 'Home.html', {'api': api})
 
 def about(request):
-	return render(request, 'about.html', {})	
+	return render(request, 'about.html', {})
