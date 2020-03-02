df5dfd0f76993754251e5db7c1dfd9ff495c7d75
Timothy Slivensky <tdslivensky@gmail.com>
2020-03-01 18:06:40 -0600

    update with about page

--- weather/weatherLookup/urls.py
+++ weather/weatherLookup/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name = "Home"),
+    path('about', views.about, name = "about"),
 ]
--- weather/weatherLookup/views.py
+++ weather/weatherLookup/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
 	return render(request, 'Home.html', {})
+
+def about(request):
+	return render(request, 'about.html', {})	
