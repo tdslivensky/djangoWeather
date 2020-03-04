9de7037e93feeb586e607d03275afc1330e1b893
Tim Slivensky <tslivensky@emailatg.com>
2020-03-04 10:09:39 -0600

    delete virtenv

--- virtenv/Scripts/activate.bat
+++ virtenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Fenrir\Documents\Github\djangoWeather\virtenv
+set VIRTUAL_ENV=C:\Users\TSlivensky\Documents\GitHub\djangoWeather\virtenv
 
 if not defined PROMPT set PROMPT=$P$G
 
