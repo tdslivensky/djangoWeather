29031aeade4a4289417c4955e819d02fe136a8d5
Tim Slivensky <tslivensky@emailatg.com>
2020-03-05 15:53:00 -0600

    Update views.py

--- weather/weatherLookup/views.py
+++ weather/weatherLookup/views.py
@@ -13,41 +13,43 @@ def Home(request):
 		#reference website for zip finder: https://pypi.org/project/uszipcode/
 		#other reference for zip finder: https://uszipcode.readthedocs.io/01-Tutorial/index.html#basic-search
 		
-		#replace by django forms input
-		zipCodeRawInformation = search.by_zipcode(request.POST['zipCode'])
+		formZip = request.POST['zipCode']
 		
-		zipCode =  zipCodeRawInformation.to_dict()
+		if formZip == "":
+			zipCodeRawInformation = search.by_zipcode('78758')
+			zipCode =  zipCodeRawInformation.to_dict()
+		else:
+			zipCodeRawInformation = search.by_zipcode(request.POST['zipCode'])
+			zipCode =  zipCodeRawInformation.to_dict()	
 		
-		#new dyanmic api
 		latitude = zipCode["lat"]
 		longitude = zipCode["lng"]
-		totalWeather = requests.get("https://api.weather.gov/points/{0},{1}".format(latitude,longitude))
 		
 		try:
-			#convert the api data to python dict
+			totalWeather = requests.get("https://api.weather.gov/points/{0},{1}".format(latitude,longitude))
+			#convert the api data which is in a request object to python dict https://stackoverflow.com/questions/48934100/how-to-convert-request-data-to-dict
 			apiTotalWeather = totalWeather.json()
 		except Exception as e:
-			apiTotalWeather = 'No information returned by apiTotalWeather'
-		
-		#make api request for general forecast
-		forecast = requests.get(apiTotalWeather["properties"]["forecast"])
-		#make api request for hourly forecast
-		hourlyForecast =  requests.get(apiTotalWeather["properties"]["forecastHourly"])
-		
-		#dump python dict back to json data
-		apiTotalWeather = json.dumps(apiTotalWeather)
-		
+			apiTotalWeather = "No information returned by apiTotalWeather"
+				
 		try:
+			#make api request for general forecast
+			forecast = requests.get(apiTotalWeather["properties"]["forecast"])
 			apiForecast = forecast.json()
-			apiForecast = json.dumps(apiForecast)
-		except Exception as e:
-			apiForecast = 'No information returned by apiForecast'
+		except Exception as f:
+			apiForecast = "No information returned by apiForecast"
 			
 		try:
+			#make api request for hourly forecast
+			hourlyForecast =  requests.get(apiTotalWeather["properties"]["forecastHourly"])
 			apiHourlyForcast = hourlyForecast.json()
-			apiHourlyForcast = json.dumps(apiHourlyForcast)
-		except Exception as e:
-			apiHourlyForcast = 'No information returned by apiHourlyForcast'
+		except Exception as g:
+			apiHourlyForcast = "No information returned by apiHourlyForcast"
+		
+		#dump python dict back to json data
+		apiHourlyForcast = json.dumps(apiHourlyForcast)
+		apiForecast = json.dumps(apiForecast)
+		apiTotalWeather = json.dumps(apiTotalWeather)	
 		
 		return render(request, 'Home.html', {'totalWeather': apiTotalWeather, 'forecast': apiForecast, 'hourlyForecast': apiHourlyForcast})
 	else:
